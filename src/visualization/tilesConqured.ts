c6471873a99da711d55338e13e288cc15bfbe533
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2026-07-09 16:43:02 -0400

    Add the option to not output video or full game heatmap for tiles conqured heatmap

--- src/visualization/tilesConqured.ts
+++ src/visualization/tilesConqured.ts
@@ -17,6 +17,13 @@ export async function createTilesConquredHeatmap(
   gameInfo: [GameStartInfo | undefined, Turn[]],
   mapLoader: gameMapLoader,
   outFolder: string,
+  options: {
+    video?: boolean;
+    fullGame?: boolean;
+  } = {
+    video: true,
+    fullGame: true,
+  },
   fileNames?: {
     video?: string;
     full?: string;
@@ -24,6 +31,12 @@ export async function createTilesConquredHeatmap(
 ) {
   if (gameInfo[0] === undefined) return;
   const gameID = gameInfo[0].gameID;
+  if (options.video === undefined) {
+    options.video = true;
+  }
+  if (options.fullGame === undefined) {
+    options.fullGame === true;
+  }
   if (fileNames === undefined) fileNames = {};
   if (fileNames.video === undefined)
     fileNames.video = "conqured-tiles-" + gameID;
@@ -39,40 +52,48 @@ export async function createTilesConquredHeatmap(
   fileNames.full = path.join(outFolder, `${fileNames.full}.png`);
   fs.mkdirSync(outFolder, { recursive: true });
   const videoData: Uint8ClampedArray[] = [];
-  const heatmapData = await heatmapMaker.create(
-    tileConqueredHandler.conqueredTilesTotal,
-  );
-  if (heatmapData) {
-    const png = new PNG({
-      width: gr.game.width(),
-      height: gr.game.height(),
-    });
-    png.data.set(heatmapData);
-
-    await new Promise<void>((resolve, reject) => {
-      if (fileNames.full)
-        png
-          .pack()
-          .pipe(fs.createWriteStream(fileNames.full))
-          .on("finish", () => resolve())
-          .on("error", reject);
-    });
-  }
-  for (let i = 0; i < tileConqueredHandler.conqueredTilesFullGame.length; i++) {
+  if (options.fullGame) {
     const heatmapData = await heatmapMaker.create(
-      tileConqueredHandler.conqueredTilesFullGame[i],
+      tileConqueredHandler.conqueredTilesTotal,
     );
-    console.log(
-      `Created heatmap ${i + 1}/${tileConqueredHandler.conqueredTilesFullGame.length}`,
-    );
-    if (heatmapData) videoData.push(heatmapData);
+    if (heatmapData) {
+      const png = new PNG({
+        width: gr.game.width(),
+        height: gr.game.height(),
+      });
+      png.data.set(heatmapData);
+
+      await new Promise<void>((resolve, reject) => {
+        if (fileNames.full)
+          png
+            .pack()
+            .pipe(fs.createWriteStream(fileNames.full))
+            .on("finish", () => resolve())
+            .on("error", reject);
+      });
+    }
   }
-  encodeVideo(
-    `${fileNames.video}`,
-    videoData,
-    gr.game.width(),
-    gr.game.height(),
-    10,
-  );
+  if (options.video) {
+    for (
+      let i = 0;
+      i < tileConqueredHandler.conqueredTilesFullGame.length;
+      i++
+    ) {
+      const heatmapData = await heatmapMaker.create(
+        tileConqueredHandler.conqueredTilesFullGame[i],
+      );
+      console.log(
+        `Created heatmap ${i + 1}/${tileConqueredHandler.conqueredTilesFullGame.length}`,
+      );
+      if (heatmapData) videoData.push(heatmapData);
+    }
+    encodeVideo(
+      `${fileNames.video}`,
+      videoData,
+      gr.game.width(),
+      gr.game.height(),
+      10,
+    );
+  }
   console.log("Done");
 }
